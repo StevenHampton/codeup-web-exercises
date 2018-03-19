76ea9acf33a107cc89829b1e0b36f0a0af220766
steven hampton <steven.hampton415@gmail.com>
2018-03-19 11:08:25 -0500

    added inline and external Javascript

---
+++ js/external.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log('Hello from external JavaScript!');
+alert("Welcome to  my Website!");
+var favcolor = prompt("what is your favorite color?");
+alert("Great, " + favcolor + " is my favorite color too!");
