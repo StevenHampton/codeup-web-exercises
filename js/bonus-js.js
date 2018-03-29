242433f0b27f059c688a49708e2209d86147ee9f
steven hampton <steven.hampton415@gmail.com>
2018-03-29 11:19:34 -0500

    finished dom query

---
+++ js/bonus-js.js
@@ -0,0 +1,25 @@
+// sum up all the expenses
+// get an average of all the expenses
+
+
+var expenses = [
+    {itemName: "macbook pro", cost: "$2,400"},
+    {itemName: "raspberry pi", cost: "$60"},
+    {itemName: "Dell XPS laptop", cost: "$1,200"}
+];
+
+function sum (expenses){
+    var total = 0;
+    expenses.forEach(function){
+
+    }
+    return total;
+}
+
+function average (sum, expenses){
+     return average = sum / expenses.length;
+}
+
+console.log(expenses);
+console.log("The sum of the expenses is $" + sum(expenses));
+console.log("The average expense is $" +average(sum(expenses)));
