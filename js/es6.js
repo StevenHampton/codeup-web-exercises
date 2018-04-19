05982fb17a3ca69cfc45f9d6db8be8c6d5b48a0d
steven hampton <steven.hampton415@gmail.com>
2018-04-19 14:09:09 -0500

    got rid of todo

--- js/es6.js
+++ js/es6.js
@@ -1,6 +1,3 @@
-/*
- * Complete the TODO items below
- */
 const users = [
   {
     name: 'zach',
@@ -29,51 +26,33 @@ const users = [
   }
 ];
 
-// TODO: fill in your name and email and add some programming languages you know
 // to the languages array
-// TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
 const name = 'steven';
 const email = 'steven.hampton415@gmail.com';
 const languages = ['html', 'css', 'javascript'];
-// TODO: rewrite the object literal using object property shorthand
 users.push({
   name,
   email,
   languages
 });
 
-// TODO: replace `var` with `let` in the following variable declarations
 let emails = [];
 let names = [];
 
-// TODO: rewrite the following using arrow functions
-users.forEach((user) =>{
-  return emails.push(user.email);
-});
-users.forEach((user) =>{
-  return names.push(user.name);
+users.forEach(user =>{
+emails.push(user.email);
+names.push(user.name);
 });
 
-// TODO: replace `var` with `let` in the following declaration
 let developers = [];
-users.forEach(function(user) {
-  // TODO: rewrite the code below to use object destructuring assignment
+users.forEach(({name, email, languages}) => {
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
-    const {name, email, languages} = user;
-
-  // TODO: rewrite the assignment below to use template strings
-  developers.push(`${name}'s email is ${email + name} knows ${languages.join(', ')}`);
+    developers.push(`${name}'s email is ${email}.  ${name} knows ${languages.join(', ')}`);
 });
 
-// TODO: Use `let` for the following variable
 let list = '<ul>';
-
-// TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
-
-  // TODO: rewrite the assignment below to use template strings
-  list += '<li>' + developer + '</li>';
-});
+for (const developer of developers) list += `<li>${developer}</li>`;
 list += '</ul>';
+
