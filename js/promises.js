89aee9155419d439ae1e86abded0e93899f3b2d2
steven hampton <steven.hampton415@gmail.com>
2018-04-20 15:50:53 -0500

    finished first problem

--- js/promises.js
+++ js/promises.js
@@ -12,3 +12,9 @@ const wait = function(number){
 };
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+fetch('https://api.github.com/users/stevenhampton/events', {headers: {'Authorization': 'token KEY'}})
+    .then((response) => response.json())
+    .then((events) => events[0])
+    .then((user) => user.created_at)
+    .then ((date) => console.log(date));
