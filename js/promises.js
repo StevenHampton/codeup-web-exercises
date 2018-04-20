bb9ad3ed91bafd6d65129e4700f16daaed5063c7
steven hampton <steven.hampton415@gmail.com>
2018-04-20 15:13:28 -0500

    finished first problem

---
+++ js/promises.js
@@ -0,0 +1,14 @@
+"use strict";
+const wait = function(number){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (number) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, number);
+    });
+};
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
