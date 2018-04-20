728409f6cd56b6d0be54d0b3e54909ed1f88687b
steven hampton <steven.hampton415@gmail.com>
2018-04-20 09:44:45 -0500

    finished first two problems of map-filter exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,40 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+const user3 = users.filter(lang => lang.languages.length >= 3);
+console.log(user3);
+
+const emailStr = user3.map(user3 => user3.email);
+console.log(emailStr);
+
+
